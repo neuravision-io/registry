a7b245f7895cd9e6a44678d4addebe0e9b950d72
Samuel Abramov <info@abramov-samuel.de>
2024-06-30 14:10:38 +0200

    first commit

---
+++ src/test/java/de/akogare/registry/RegistryApplicationTests.java
@@ -0,0 +1,11 @@
+package de.akogare.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
